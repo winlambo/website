0eb45071a7294bef909ac721db35afe522fb5097
deazyyy <d.s@vimerco.com>
2021-06-01 17:30:58 +0530

    change

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -254,7 +254,7 @@ section {
 }
 
 .bg-black {
-	background: #000;
+	background-color: #000;
 	color: #fff;
 	padding: 20vh 0;
 }
@@ -291,6 +291,7 @@ nav {
 	padding: 10px 0;
 
 	.logo {
+		position:static;
 		height: 30px;
 		width: auto;
 	}
@@ -500,6 +501,79 @@ nav {
 }
 
 
+
+/* volume section */
+.chad-bg{
+	background: url(/images/chadr.png) 102% bottom no-repeat, url(/images/chadl.png) -2% bottom no-repeat;
+		background-size: auto 50%,auto 50%;
+	position:relative;
+	padding: 5vh 0 20vh;
+	&:before{
+		position:absolute;
+		content:"";
+		background:#000;
+		height:100%;
+		width:100%;
+		top:0;
+		z-index:-1
+	}
+	.chadheading{
+		font-size:40px;
+		line-height: 38px;
+		font-weight:700;
+		display:flex;
+		text-align:right;
+		margin-bottom:60px;
+		.chadheadingoter{
+			margin:auto;
+		}
+		.chadchadbg{
+			color:#444444;
+			display:flex;
+			justify-content: flex-end;	
+			font-weight: 700;
+			span{
+				margin-left:10px;
+				position:relative;
+				top:-4px;
+				font-weight: 700;				
+			}	
+			&.chadbottom{
+				position:relative;
+				right:-30px;
+			}
+		}
+	}
+	.chadcols{
+		padding:0 20px 100px;
+		div{
+			color:#feffff;	
+			font-weight:300;
+		}
+		h1{
+			font-size:28px;
+		}
+	}
+
+	.chadcol1{
+		background:url(/images/col1.png);
+		background-size: 75%;
+		background-repeat: no-repeat;
+		background-position: center bottom;
+	}
+	.chadcol2{
+		background:url(/images/col2.png);
+		background-size: 75%;
+		background-repeat: no-repeat;
+		background-position: center bottom;
+	}
+	.chadcol3{
+		background:url(/images/col3.png);
+		background-size: 75%;
+		background-repeat: no-repeat;
+		background-position: center bottom;
+	}
+}
 
 
 
--- src/views/components/Volume.tsx
+++ src/views/components/Volume.tsx
@@ -5,35 +5,51 @@ import React from 'react';
 
 const Volume: React.FC = () => {
     return (
-        <section className="bg-black">
+        <section className="bg-black chad-bg">
             <div className="container">
+                <div className="chadheading">
+                    <div className="chadheadingoter">
+                        <div className="chadchadbg chadupper">chad <span>chad</span></div>
+                        Why become a chad?
+                        <div className="chadchadbg chadbottom">chad <span>chad</span></div>
+                    </div>
+                </div>
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-md-4 chadcols chadcol1">
                         <h1>Fair-Winner System</h1>
                         <div>
-                            When it is time to send out a Lambo, we will use Chainlink's random number generator to generate the winning ticket! This is fair and verifiable, on-chain! <br /><br />
-                            Winners will be announced on our Telegram! <br /><br />
-                            Learn more about the Lambo drawing:
-                            <div className="btn-outer">
-                                <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x6a79e08db6c08b8f88703794bf1a47f5a01eb9dc" className="btn-main btn-white">Buy Now<i className="fas fa-mouse-pointer"></i></a>
-                                <a href="https://winlambo.medium.com/" className="btn-main btn-white"><i className="fab fa-medium" />Learn More</a>
-                                <a href="https://t.me/winlambo_official" className="btn-main btn-white"><i className="fas fa-paper-plane"></i>Telegram</a>
-                            </div>
+                        When it is time to send out a Lambo, 
+                        we will use Chainlink's random numbe
+                        r generator to generate the winning ticket! This is fair and verifiable, on-chain! <br /><br />
+                        Winners will be announced on our Telegram!
+                        Learn more about the Lambo drawing:
                         </div>
                     </div>
-                    <div className="col-md-6 mt-md-0 mt-sm-3">
+                    <div className="col-md-4 mt-md-0 mt-sm-3 chadcols chadcol2">
                         <h1>Incentivized Volume</h1>
                         <div>
                             <div>
-                                We plan on pumping out Lambos by incentivizing volume with a Daily Jackpot! The Daily jackpot will be distributed to winners in 2 ways:
+                             We plan on pumping out Lambos by incentivizing volume with a Daily Jackpot! The Daily jackpot will be distributed to winners in 2 ways:
                             </div>
                             <ul className="my-3 ml-3">
-                                <li>30% will go to the top 3 players in terms of volume for that day.</li>
-                                <li>70% will go to 7 lucky players.</li>
+                                <li>70% will to the top 3 aily volume users </li>
+                                <li>30% will go to 6 lucky users.</li>
                             </ul>
 
                             <div>
-                                The more transactions performed, the more Lambos we can giveaway to $WINLAMBO token holders!
+                            The more transactions performed, the more Lambos we can giveaway to $WINLAMBO token holders!
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mt-md-0 mt-sm-3 chadcols chadcol3">
+                        <h1>Community Driven</h1>
+                        <div>
+                            <div>
+                                We are a fully community driven and orientated project. We are build for chads by chads.
+                            </div>
+                            <br />
+                            <div>
+                                Our marketing and different positons are filled by community members and highly dedicated individuals to ensure maximum project growht and success.
                             </div>
                         </div>
                     </div>
